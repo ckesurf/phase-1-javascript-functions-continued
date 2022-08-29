e6a1c6d13cd81a7882257f65817b8da8f4f52031
Christopher K Erlendson <christopherkerlendson@Christophers-MacBook-Pro.local>
2022-08-29 13:55:27 -0400

    commit tree 3ccbe6af333e7fc7d7a103a3c9d2c0feb81be181

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun(); //=> "This Saturday, I want to roller-skate!"
+
+saturdayFun("bathe my dog"); //=> "This Saturday, I want to bathe my dog!"
